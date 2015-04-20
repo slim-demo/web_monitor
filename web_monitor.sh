2525a5dc7ff14d5c89b21f7bccbaf967fba155de
Nadim Slim <slim_demo@adsen.com>
2015-04-20 07:14:08 -0400

    Adds the timestamp to the log

--- web_monitor.sh
+++ web_monitor.sh
@@ -14,13 +14,13 @@ else
   httpd_check=`ps aux | grep [h]ttpd | wc -l`
   if [ $httpd_check -lt 10 ]
   then
-    echo "[LOW] Web Server OK!"
+    echo "$(date) - [LOW] Web Server OK!"
   elif [[ $httpd_check -gt 20 && $httpd_check -lt 100 ]]
   then
-    echo "[HIGH] Web Server Working hard!"
+    echo "$(date) - [HIGH] Web Server Working hard!"
   elif [ $httpd_check -gt 100 ]
   then
-    echo "[CRITICAL] Web Server under heavy load, restart required"
+    echo "$(date) - [CRITICAL] Web Server under heavy load, restart required"
    systemctl restart httpd.service 
   else
     exit 0
